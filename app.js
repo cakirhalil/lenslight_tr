24e44bd914ab69074252faee3658994c6bf5a290
cakirhalil <halilcakir343@gmail.com>
2024-07-22 23:28:40 +0300

    lesson 02-03

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ import express from 'express';
 const app= express();
 const port = 3000;
 
+//ejs template engine
+app.set('view engine', 'ejs');
+
+
+//static files midleware
+app.use(express.static('public'));
+
 app.get("/", (req, res)=>{
-    res.send("index sayfası2")
-})
+    res.render('index');
+});
+
+app.get("/about", (req, res)=>{
+    res.render('about');
+});
 
 app.listen(port, () => {
     console.log(`Aplication running on port: ${port}`);
